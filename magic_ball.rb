54f3e4426bb814365b5773a5d31a78899f5590a6
Isaac Saldivar <isaacsaldivar@Isaacs-MacBook-Pro.local>
2016-05-12 15:05:03 -0600

    update magic 8 ball

--- magic_ball.rb
+++ magic_ball.rb
@@ -18,32 +18,40 @@
 def magic_eight_menu
 	puts "Welcome to the Magic 8 Ball"
 	puts "Please ask your first question or type QUIT to exit!"
-  user_input = gets.to.if condition
+  user_input = gets.strip.downcase
+  exit (0) if user_input == "quit"
+ 	output
   	
-  end
-def answers
-	answers = ["It is certain",
-"It is decidedly so",
-"Without a doubt",
-"Yes, definitely",
-You may rely on it
-As I see it, yes
-Most likely
-Outlook good
-Yes
-Signs point to yes
-Reply hazy try again
-Ask again later
-Better not tell you now
-Cannot predict now
-Concentrate and ask again
-Don't count on it
-My reply is no
-My sources say no
-Outlook not so good
-Very doubtful]
 end
 
+	@arr = ["It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes, definitely",
+		"You may rely on it",
+		"As I see it, yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",ß
+		"My reply is no",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful",]
+ 
+
+	def output
+		puts @arr.sample
+		
+  end
+
+magic_eight_menu
 
 
 
@@ -71,6 +79,4 @@ end
 
 
 
-
-def magic_eight_menu
 
